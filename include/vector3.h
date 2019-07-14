f90f0cab7a6b9c50d470519f7659b4a55a4c7f90
duwenjie <duwenjie@firevale.com>
2019-07-14 21:26:04 +0800

    wip

--- include/vector3.h
+++ include/vector3.h
@@ -49,7 +49,7 @@ namespace physics
       return *this;
     }
 
-    Vector3 & operator + (const Vector3 &other)
+    Vector3 operator + (const Vector3 &other)
     {
       return Vector3(x + other.x, y + other.y, z + other.z);
     }
@@ -61,7 +61,7 @@ namespace physics
       z += other.z;
     }
 
-    Vector3 & operator - (const Vector3 &other)
+    Vector3 operator - (const Vector3 &other)
     {
       return Vector3(x - other.x, y - other.y, z - other.z);
     }
@@ -73,20 +73,75 @@ namespace physics
       z -= other.z;
     }
 
-    Vector3 & operator * (const ffloat &scale)
+    Vector3 scale (const ffloat &scale)
     {
       return Vector3(x * scale, y * scale, z * scale);
     }
 
-    void operator *= (const ffloat &scale)
+    Vector3 scaleUpdate(const ffloat &scale)
     {
       x *= scale;
       y *= scale;
       z *= scale;
     }
 
+    //component-wise product with other vector
+    void product(const Vector3 *other)
+    {
+      return Vector3(x * other.x, y * other.y, z * other.z);
+    }
     
+    void productUpdate(const Vector3 *other)
+    {
+      x *= other.x;
+      y *= other.y;
+      z *= other.z;
+    }
+
+    ffloat dot(const Vector3 &other)
+    {
+      return x*other.x + y*other.y + z*other.z;
+    }
+
+
+    Vector3 cross(const Vector3 &other)
+    {
+      return Vector3(y*other.z-z*other.y, 
+                     z*other.x-x*other.z, 
+                     x*other.y-y*other.x);
+    }
+
+    void crossUpdate(const Vector3 &other)
+    {
+      x = y*other.z-z*other.y;
+      y = z*other.x-x*other.z;
+      z = x*other.y-y*other.x;
+    }
+
+    //Performs cross-product A x B x C = B x (C . A) - A x (C . B)
+    static Vector3 tripleCross(const Vector3 &A, const Vector3 &B, const Vector3 &C)
+    {
+      return (B.scale(C.dot(A))) - (A.scale(C.dot(B)))
+    }
+
+    void addScaleVector(const Vector3 &other, ffloat scale)
+    {
+      x += other.x * scale;
+      y += other.y * scale;
+      z += other.z * scale;
+    }
+
+    ffloat mag() const
+    {
+      return ffsqrt(x*x + y*y + z*z);
+    }
+
+    ffloat sqearMag() const
+    {
+      return x*x + y*y + z*z;
+    }
   }
+}
 }
 
 #endif
