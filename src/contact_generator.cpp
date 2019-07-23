d418ce600a8f465ac37b7971ac6980dbd2e35da6
du wenjie <duwenjie@gmail.com>
2019-07-24 01:40:57 +0800

    wip

--- src/contact_generator.cpp
+++ src/contact_generator.cpp
@@ -153,14 +153,14 @@ namespace physics
       if (dist > sphere.radius)
         return 0;
 
-      ffloat pen = sphere.radius - ffabs(dist);
+      ffloat penetration = sphere.radius - ffabs(dist);
       Vector3 normal = dirWrold;
       if(dist < ffzero)
         normal = -normal;
 
       Contact* contact = cData->nextContact;
       contact->contactNormal = normal;
-      contact->penetration = pen;
+      contact->penetration = penetration;
       contact->contactPoint = pp;
       contact->setBodyData(sphere.body, 0, cData->friction, cData->restitution);
       cData->addContacts(1);
