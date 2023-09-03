20d2aac52318b84a7feeb335a7b18aa0ff7e0f7b
duwenjie <duwenjie@gmail.com>
2023-09-03 15:58:38 +0800

    wip

--- projects/wasm/cocos/assets/scripts/app.ts
+++ projects/wasm/cocos/assets/scripts/app.ts
@@ -11,8 +11,14 @@ import { assetManager, BufferAsset } from 'cc';
 export class entry extends Component {
     @property(BufferAsset)
     wasmBuffer: BufferAsset = null;
+
+    wasmModule: any = null;
     start() {
-        this.loadWasm();
+        this.loadWasm().then((module) => {
+            this.wasmModule = module;
+            let test: TestPhysicWasm = new TestPhysicWasm(module);
+            test.testWasm();
+        })
 
     }
 
@@ -20,17 +26,19 @@ export class entry extends Component {
 
     }
 
-    loadWasm() {
-        let wasmBinary = new Uint8Array(this.wasmBuffer.buffer());
-        factory({
-            wasmBinary
-        }).then((module) => {
-            console.log(">>>>>module:", module);
-            let test: TestPhysicWasm = new TestPhysicWasm(module);
-            test.testWasm();
-        });
-
-
+    loadWasm(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            try {
+                let wasmBinary = new Uint8Array(this.wasmBuffer.buffer());
+                factory({
+                    wasmBinary
+                }).then((module) => {
+                    resolve(module);
+                });
+            } catch (error) {
+                reject('init wasm failed!')
+            }
+        })
     }
 }
 
